f714e13aedba4692d31c84e2b05fbdcd5d33bd62
Dimitri Hooftman <Hooftman.D@gmail.com>
2018-04-10 16:53:47 +0200

    Profile page

--- src/public/js/login.js
+++ src/public/js/login.js
@@ -4,15 +4,14 @@ function onSignIn(googleUser) {
     // Useful data for your client-side scripts:
     var profile = googleUser.getBasicProfile();
 
-    // // The ID token you need to pass to your backend:
+    // The ID token you need to pass to your backend:
     var id_token = googleUser.getAuthResponse().id_token;
-    console.log("ID Token: " + id_token);
 
     var xhr = new XMLHttpRequest();
     xhr.open('POST', URL_AUTHENTICATION_SERVICE);
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.onload = function() {
-        console.log('Signed in as: ' + xhr.responseText);
+        //console.log('Signed in as: ' + xhr.responseText);
     };
     xhr.send(JSON.stringify({idtoken: id_token}));
-};
+}
